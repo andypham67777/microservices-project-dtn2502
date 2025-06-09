91143974e75433b48b230704d74e9fe8a1a828f7
andypham67777 <nganpham1303@gmail.com>
2025-06-09 11:48:01 +0700

    Init project

---
+++ account-service/src/main/java/vti/dtn/account_service/controller/AccountController.java
@@ -0,0 +1,22 @@
+package vti.dtn.account_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vti.dtn.account_service.dto.AccountDTO;
+import vti.dtn.account_service.services.AccountService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "api/v1/accounts")
+public class AccountController {
+    private final AccountService accountService;
+
+    @GetMapping
+    public List<AccountDTO> getListAccounts() {
+        return accountService.getListAccounts();
+    }
+}
